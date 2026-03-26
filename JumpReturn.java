09712c09863acb253b7f3dd773ff78cf340ace1e
https://github.com/lPragathi <lpragathi595@gmail.com>
2026-03-26 14:34:19 +0530

    Updated jumpreturn structures

---
+++ JumpReturn.java
@@ -0,0 +1,9 @@
+public class JumpReturn {
+    public static void main(String[] args) {
+        System.out.println("Before return staement");
+        //return;
+        //this is not allowed and gives error
+        //System.out.println("Error after return staement");
+    }
+    
+}
---
+++ Primenumber.java
@@ -0,0 +1,7 @@
+public class Primenumber {
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
