a327067266c7739abf1c5f5a6e89b8d56658f51d
https://github.com/lPragathi <lpragathi595@gmail.com>
2026-03-26 13:11:56 +0530

    Updated jumpbreak structures

--- JumpBreak.java
+++ JumpBreak.java
@@ -1,8 +1,9 @@
-public class Jump {
+public class JumpBreak {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++){
             if(i==5)
-                break;
+                //break;
+            continue;
             System.out.println("value is:" +i);
         }
         
