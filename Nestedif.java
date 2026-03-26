6b7eb74c18dcd857ea0e0bef62562106c7f65520
https://github.com/lPragathi <lpragathi595@gmail.com>
2026-03-26 12:13:15 +0530

    Updated nestedif structures

--- Nestedif.java
+++ Nestedif.java
@@ -5,14 +5,14 @@ public class Nestedif {
         // age = 65;
         System.out.println("Enter the age");
         int age = sc.nextInt();
-        
+
         if(age>=18){
             if(age>=65){
-                System.out.println("Eligible to avail government fund");
+                System.out.println("Eligible to  vote a avail government fund");
             }else
-                System.out.println("We are not eligible for government scheme");
+                System.out.println("We are eligible to vote but not eligible avail government scheme");
         } else
-        System.out.println("We are not eligible to vote");
+        System.out.println("We are not eligible to vote and not eligible to avail government scaheme");
 
     }
 }
